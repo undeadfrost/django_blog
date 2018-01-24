821ef4bd312d64a3626a289b714be93f0593a1cf
undeadfrost <undeadfrost@live.com>
2018-01-24 11:32:17 +0800

    创建视图模板

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
 
 
 class Category(models.Model):
@@ -14,6 +15,10 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    @python_2_unicode_compatible
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     """
@@ -22,6 +27,9 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    @python_2_unicode_compatible
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     """
@@ -58,4 +66,7 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html', context={
+                      'title': '我的博客首页', 
+                      'welcome': '欢迎访问我的博客首页'
+                  })
+

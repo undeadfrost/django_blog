264dce4c4423a5ad6b67344b2dde35b46a829569
undeadfrost <undeadfrost@live.com>
2018-01-25 19:48:33 +0800

    添加评论模型及视图

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ import markdown
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'index.html', context={
+    return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
 
@@ -20,16 +20,16 @@ def info(request, pk):
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
     ])
-    return render(request, 'info.html', context={'post': post})
+    return render(request, 'blog/info.html', context={'post': post})
 
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month).order_by('-created_time')
-    return render(request, 'index.html', context={'post_list': post_list})
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'index.html', context={'post_list': post_list})
+    return render(request, 'blog/index.html', context={'post_list': post_list})

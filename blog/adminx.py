f36ac28c14eb434266053939331f5e279ebb2946
Frost <undeadfrost@live.com>
2018-02-04 23:32:30 +0800

    修改界面及xadmin

--- blog/adminx.py
+++ blog/adminx.py
@@ -7,14 +7,14 @@ from .models import Category, Tag, Post
 class GlobalSetting(object):
     site_title = '博客管理'
     site_footer = '博客管理系统'
+    # 左侧折叠菜单
+    # menu_style = "accordion"
 
 
 class PostAdmin(object):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
     search_fields = ['title', 'category__name']
     style_fields = {'tags': 'm2m_transfer'}
-    # style_fields = ['tags']
-    # raw_id_fields = ['category']
 
 
 xadmin.site.register(views.CommAdminView, GlobalSetting)
